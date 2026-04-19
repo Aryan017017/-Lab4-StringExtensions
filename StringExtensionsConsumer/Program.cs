e76e9c16d28062c8b450fa298e033af9749a448a
Aryankumar Chaudhari <85046770+Aryan017017@users.noreply.github.com>
2026-04-18 22:11:26 -0400

    Add console app

---
+++ StringExtensionsConsumer/Program.cs
@@ -0,0 +1,7 @@
+﻿using StringExtensions;
+
+string test1 = "Hello World";
+string test2 = "hello world";
+
+Console.WriteLine($"{test1} starts with uppercase: {test1.StartsWithUpperCase()}");
+Console.WriteLine($"{test2} starts with uppercase: {test2.StartsWithUpperCase()}");
